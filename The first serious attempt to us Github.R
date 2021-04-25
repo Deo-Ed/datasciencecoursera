8a4e7880c8fc0acceedb9cfe78b5cd039d0159f4
Deodat Mwesiumo <mwesiumod@gmail.com>
2021-04-25 12:49:30 +0200

    Update The first serious attempt to us Github.R

--- The first serious attempt to us Github.R
+++ The first serious attempt to us Github.R
@@ -1,3 +1,5 @@
-print("this file was created withi Rstudio")
+print("this file was created within Rstudio")
 
 print("and now it lives on Github")
+
+print("see if you can pull the script from Github")
