6da4e1eee8131bbb74b0c7d511c0b6bac2b2b85d
Deo-Ed <mwesiumod@gmail.com>
2021-04-25 13:01:38 +0200

    I have added a few lines of code

--- The first serious attempt to us Github.R
+++ The first serious attempt to us Github.R
@@ -3,3 +3,20 @@ print("this file was created within Rstudio")
 print("and now it lives on Github")
 
 print("see if you can pull the script from Github")
+
+
+mean(c(3, 6, 9))
+
+x <- c(20, 30, 40, 50, 60, 70, 80, 90, 100)
+
+y <- c(100, 150, 200, 250, 300, 350, 400, 450, 200)
+
+dataxy <- merge(x, y)
+
+
+pesa <- dataxy$x
+
+
+mauzo <- dataxy$y
+
+cor(x, y)
